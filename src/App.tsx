0427653057daf0043bf3087296ef3fd74f986322
Kaido Iwamoto <h6824.kaido@gmail.com>
2019-07-14 11:38:55 +0900

    Implement authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useLayoutEffect, useState, useCallback } from 'react'
 import styled from '@emotion/styled'
+import { AuthDisplay } from './AuthDisplay'
 
 const Board = styled.svg`
   width: 700px;
@@ -30,12 +31,13 @@ export function App() {
 
   useLayoutEffect(() => {
     const onMouseUp = (e: MouseEvent) => {
-      console.log('up!')
-      memo.current.isMouseDown = false
-      setPaths(
-        memo.current.paths.concat([{ color, width: lineWidth, points: memo.current.points }])
-      )
-      memo.current.points = []
+      if (memo.current.isMouseDown) {
+        memo.current.isMouseDown = false
+        setPaths(
+          memo.current.paths.concat([{ color, width: lineWidth, points: memo.current.points }])
+        )
+        memo.current.points = []
+      }
     }
     document.addEventListener('mouseup', onMouseUp)
 
@@ -45,30 +47,34 @@ export function App() {
   }, [])
 
   return (
-    <Board
-      ref={boardRef}
-      onMouseDown={useCallback((e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
-        memo.current.isMouseDown = true
-      }, [])}
-      onMouseMove={useCallback((e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
-        if (memo.current.isMouseDown) {
-          const {
-            nativeEvent: { offsetX: x, offsetY: y }
-          } = e
-          const { points } = memo.current
-          const lastPoint = points.length > 0 ? points[points.length - 1] : null
-          if (lastPoint == null || lastPoint.x !== x || lastPoint.y !== y) {
-            memo.current.points.push({ x, y })
-            setDrawingPath({ color, width: lineWidth, points: memo.current.points })
+    <div>
+      <h1>draw</h1>
+      <AuthDisplay />
+      <Board
+        ref={boardRef}
+        onMouseDown={useCallback((e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+          memo.current.isMouseDown = true
+        }, [])}
+        onMouseMove={useCallback((e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+          if (memo.current.isMouseDown) {
+            const {
+              nativeEvent: { offsetX: x, offsetY: y }
+            } = e
+            const { points } = memo.current
+            const lastPoint = points.length > 0 ? points[points.length - 1] : null
+            if (lastPoint == null || lastPoint.x !== x || lastPoint.y !== y) {
+              memo.current.points.push({ x, y })
+              setDrawingPath({ color, width: lineWidth, points: memo.current.points })
+            }
           }
-        }
-      }, [])}
-    >
-      {paths.map((p, i) => (
-        <PathComponent key={i} path={p} />
-      ))}
-      {drawingPath && <PathComponent path={drawingPath} />}
-    </Board>
+        }, [])}
+      >
+        {paths.map((p, i) => (
+          <PathComponent key={i} path={p} />
+        ))}
+        {drawingPath && <PathComponent path={drawingPath} />}
+      </Board>
+    </div>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import { App } from './App'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAHYun-sXC5ZkK-XNpEqaImnd24nxrCbLM',
+  authDomain: 'draw-9a1e4.firebaseapp.com',
+  databaseURL: 'https://draw-9a1e4.firebaseio.com',
+  projectId: 'draw-9a1e4',
+  storageBucket: 'draw-9a1e4.appspot.com',
+  messagingSenderId: '251397130642',
+  appId: '1:251397130642:web:ae3b9c1dff554eea'
+})
 
 render(<App />, document.getElementById('app'))
