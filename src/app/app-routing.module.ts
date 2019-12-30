4d1d59b1098a0c84eed90b330c66b44f5a19b000
Kaido Iwamoto <h6824.kaido@gmail.com>
2019-12-30 23:26:12 +0900

    Fix compile error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core'
-import { Routes, RouterModule, UrlMatchResult } from '@angular/router'
+import { Routes, RouterModule, UrlMatchResult, UrlSegment } from '@angular/router'
 import { DrawingScreenComponent } from './drawing-screen/drawing-screen.component'
+
+export function pictureIdMatcher(segments: UrlSegment[]): UrlMatchResult {
+  if (segments.length === 1 && segments[0].path.match(/^[0-9a-f]{32}$/)) {
+    return { consumed: segments, posParams: { pictureId: segments[0] } }
+  }
+  return (null as unknown) as UrlMatchResult
+}
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: DrawingScreenComponent },
   { path: 'p/:pictureId', redirectTo: ':pictureId' },
   {
-    matcher: (segments, group, route) => {
-      if (segments.length === 1 && segments[0].path.match(/^[0-9a-f]{32}$/)) {
-        return { consumed: segments, posParams: { pictureId: segments[0] } }
-      }
-      return (null as unknown) as UrlMatchResult
-    },
+    matcher: pictureIdMatcher,
     component: DrawingScreenComponent
   }
 ]
