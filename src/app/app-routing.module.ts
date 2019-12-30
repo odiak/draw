52aad04e69f56a6402c064b79f938d1b3d02cc4c
Kaido Iwamoto <h6824.kaido@gmail.com>
2019-12-30 23:14:32 +0900

    Change routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core'
-import { Routes, RouterModule } from '@angular/router'
+import { Routes, RouterModule, UrlMatchResult } from '@angular/router'
 import { DrawingScreenComponent } from './drawing-screen/drawing-screen.component'
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: DrawingScreenComponent },
-  { path: 'p/:pictureId', component: DrawingScreenComponent }
+  { path: 'p/:pictureId', redirectTo: ':pictureId' },
+  {
+    matcher: (segments, group, route) => {
+      if (segments.length === 1 && segments[0].path.match(/^[0-9a-f]{32}$/)) {
+        return { consumed: segments, posParams: { pictureId: segments[0] } }
+      }
+      return (null as unknown) as UrlMatchResult
+    },
+    component: DrawingScreenComponent
+  }
 ]
 
 @NgModule({
