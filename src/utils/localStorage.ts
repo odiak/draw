0aa237bac3cfaf8787e70efc63a3c50c9aed0e50
Kaido Iwamoto <h6824.kaido@gmail.com>
2020-10-22 17:37:07 +0900

    Store data on dummy localStorage

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,15 +1,33 @@
-const dummyLocalStorage: Storage = {
-  length: 0,
-  clear() {},
-  getItem() {
-    return null
-  },
-  key() {
-    return null
-  },
-  setItem() {},
-  removeItem() {}
+class DummyLocalStorage implements Storage {
+  private data: { [k: string]: string } = {}
+
+  get length(): number {
+    return Object.getOwnPropertyNames(this.data).length
+  }
+
+  clear(): void {
+    this.data = {}
+  }
+
+  key(index: number): string | null {
+    const keys = Object.getOwnPropertyNames(this.data)
+    return keys[index] ?? null
+  }
+
+  getItem(key: string): string | null {
+    return this.data[key] ?? null
+  }
+
+  setItem(key: string, value: string): void {
+    this.data[key] = value
+  }
+
+  removeItem(key: string): void {
+    delete this.data[key]
+  }
 }
+
+const dummyLocalStorage = new DummyLocalStorage()
 
 export const localStorage = (() => {
   try {
