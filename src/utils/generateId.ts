d4b7dba3133440fc8a469570f6b41e05e5262a39
takker99 <37929109+takker99@users.noreply.github.com>
2021-08-27 11:02:24 +0900

    :pencil: generateId.ts

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -1,3 +1,4 @@
+/** ランダムな32桁の16進数で構成された文字列を生成する関数 */
 export function generateId(): string {
   return Array.from(crypto.getRandomValues(new Uint8Array(16)))
     .map((n) => n.toString(16).padStart(2, '0'))
