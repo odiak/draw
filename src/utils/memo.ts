979d49c4a9a2576aea73c5f94cb86941fb59a25b
takker99 <37929109+takker99@users.noreply.github.com>
2021-08-27 11:03:07 +0900

    :pencil: memo.ts

--- src/utils/memo.ts
+++ src/utils/memo.ts
@@ -1,5 +1,11 @@
 const uninitialized: unknown = {}
 
+/** 関数の実行結果をcacheするやつ
+ *
+ * 2回目の呼び出し以降は、1回目の実行結果を返す
+ *
+ * @param 実行したい関数
+ */
 export function memo<T>(f: () => T): () => T {
   let val: T = uninitialized as T
   return () => {
