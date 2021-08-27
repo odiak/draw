c2c881df644f3387960e45c3d9e4b78b4065dbfa
takker99 <37929109+takker99@users.noreply.github.com>
2021-08-27 11:02:05 +0900

    :pencil: Subject.ts

--- src/utils/Subject.ts
+++ src/utils/Subject.ts
@@ -3,12 +3,21 @@ export type Callback<T> = (value: T) => void
 export class Subject<T> {
   private callbacks: Array<Callback<T>> = []
 
+  /** 登録された関数を同じ引数を渡して全て実行する
+   *
+   * @param 登録された関数に渡す引数
+   */
   next(value: T) {
     for (const c of this.callbacks) {
       c(value)
     }
   }
 
+  /** 関数を登録する
+   *
+   * @param 登録する関数
+   * @return 登録解除用コールバック
+   */
   subscribe(callback: Callback<T>): () => void {
     this.callbacks.push(callback)
 
@@ -17,6 +26,12 @@ export class Subject<T> {
     }
   }
 
+  /** 関数の登録を解除する
+   *
+   * 未登録の関数が渡された場合は何もしない
+   *
+   * @param 登録を解除したい関数
+   */
   unsubscribe(callback: Callback<T>) {
     const i = this.callbacks.indexOf(callback)
     if (i !== -1) {
