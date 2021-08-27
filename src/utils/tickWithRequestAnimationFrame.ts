58378a611ed35d445f49e2f803c09f79cba13dfa
takker99 <37929109+takker99@users.noreply.github.com>
2021-08-27 11:03:20 +0900

    :pencil: tickWithRequestAnimationFrame.ts

--- src/utils/tickWithRequestAnimationFrame.ts
+++ src/utils/tickWithRequestAnimationFrame.ts
@@ -1,3 +1,9 @@
+/** 画面の再描画ごとに1回以下しか実行できないようにするwrapper
+ *
+ * 何回実行しても、1回の再描画につき最大1回しか実行されないようにする
+ *
+ * @param f 再描画毎に実行したい関数
+ */
 export function tickWithRequestAnimationFrame(f: () => void): () => void {
   let ticking = false
 
