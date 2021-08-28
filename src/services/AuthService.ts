175d6c9ec102ae644b2dd331e5e5e1585e2fdd46
Kaido Iwamoto <h6824.kaido@gmail.com>
2021-08-28 10:29:47 +0900

    Fix sign-in problem on Safari

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -27,12 +27,36 @@ export class AuthService {
     })
   }
 
-  async signInWithGoogle(): Promise<firebase.auth.UserCredential | null> {
-    const cred = await this.auth.signInWithPopup(this.googleAuthProvider).catch(() => null)
+  async signInWithGoogle(usePopup: boolean = true): Promise<firebase.auth.UserCredential | null> {
+    if (!usePopup) {
+      await this.auth.signInWithRedirect(this.googleAuthProvider)
+      return null
+    }
+
+    const cred = await this.auth.signInWithPopup(this.googleAuthProvider).catch((e: unknown) => {
+      console.error(e)
+      const message = extractMessage(e)
+      if (
+        usePopup &&
+        message != null &&
+        /\bpopup\b/i.test(message) &&
+        /\bblocked\b/i.test(message)
+      ) {
+        return this.signInWithGoogle(false)
+      }
+      return null
+    })
     return cred
   }
 
   async signOut(): Promise<void> {
     await this.auth.signOut()
   }
+}
+
+function extractMessage(error: unknown): string | undefined {
+  if (error == null || typeof error !== 'object') return
+  const { message } = error as Record<string, unknown>
+  if (typeof message !== 'string') return
+  return message
 }
