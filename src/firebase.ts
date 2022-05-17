f7ffc53161ef26ffade7f34a8bc8bcb181332164
Kaido Iwamoto <kaido@odiak.net>
2022-05-18 02:26:19 +0900

    Fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,7 +3,7 @@ import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check'
 
 const app = initializeApp(JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG!))
 
-if (process.env.NEXT_PUBLIC_RECAPTCHA_KEY) {
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_RECAPTCHA_KEY) {
   initializeAppCheck(app, {
     provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_RECAPTCHA_KEY),
     isTokenAutoRefreshEnabled: true
