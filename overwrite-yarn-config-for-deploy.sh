35b3903d9f831d28fd0c2f3636b5ee313107da95
Kaido Iwamoto <kaido@odiak.net>
2023-08-13 03:52:14 +0900

    Fix

--- overwrite-yarn-config-for-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -Eeuo pipefail
-
-if [ -n "$FONTAWESOME_NPM_AUTH_TOKEN" ]; then
-    yarn config set npmScopes.fortawesome.npmRegistryServer 'https://npm.fontawesome.com'
-    yarn config set npmScopes.fortawesome.npmAuthToken "$FONTAWESOME_NPM_AUTH_TOKEN"
-fi
