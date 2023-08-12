b39dd32540938cf1714f70d799fe6ea34745907f
Kaido Iwamoto <kaido@odiak.net>
2023-08-13 02:55:57 +0900

    Modify config

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,28 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    kakeruSecrets: fs.readFileSync('./secrets.json', 'utf-8'),
-    isRecaptchaEnabled: true
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor'
+console.log(process.env.NODE_ENV)
+export default defineConfig((env) => {
+  const kakeruSecrets = fs.readFileSync('./secrets.json', 'utf-8')
+  const isRecaptchaEnabled = env.mode === 'production'
+
+  return {
+    plugins: [react()],
+    define: {
+      kakeruSecrets,
+      isRecaptchaEnabled
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('node_modules')) {
+              return 'vendor'
+            }
           }
         }
-      }
-    },
-    chunkSizeWarningLimit: 1000
+      },
+      chunkSizeWarningLimit: 1000
+    }
   }
 })
