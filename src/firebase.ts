470841ac9821153828eaf278d2c7d4327e3abe44
Kaido Iwamoto <kaido@odiak.net>
2023-08-19 16:23:10 +0900

    Enable AppCheck on local

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,9 +3,10 @@ import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check'
 
 const app = initializeApp(kakeruSecrets.firebaseConfig)
 
-if (isRecaptchaEnabled) {
-  initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider(kakeruSecrets.recaptchaKey),
-    isTokenAutoRefreshEnabled: true
-  })
+if (!isRecaptchaEnabled && kakeruSecrets.appCheckDebugToken) {
+  ;(window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = kakeruSecrets.appCheckDebugToken
 }
+initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(kakeruSecrets.recaptchaKey),
+  isTokenAutoRefreshEnabled: true
+})
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,2 +1,7 @@
-declare const kakeruSecrets: { firebaseConfig: any; sentryDsn: string; recaptchaKey: string }
+declare const kakeruSecrets: {
+  firebaseConfig: any
+  sentryDsn: string
+  recaptchaKey: string
+  appCheckDebugToken?: string | boolean
+}
 declare const isRecaptchaEnabled: boolean
