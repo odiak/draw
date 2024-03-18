b65e218e1b924020d99bc64ee54a67a2b75cb6c7
Kaido Iwamoto <kaido@odiak.net>
2024-03-18 13:29:00 +0900

    Append meta tags with env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
+import { PluginOption, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
-console.log(process.env.NODE_ENV)
 export default defineConfig((env) => {
   const kakeruSecrets = fs.readFileSync('./secrets.json', 'utf-8')
   const isRecaptchaEnabled = env.mode === 'production'
 
   return {
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), modifyHtml()],
     define: {
       kakeruSecrets,
       isRecaptchaEnabled
@@ -29,3 +28,15 @@ export default defineConfig((env) => {
     }
   }
 })
+
+function modifyHtml(): PluginOption {
+  return {
+    name: 'modify-html',
+    transformIndexHtml(html) {
+      const metaTags = process.env.KAKERU_META_TAGS ?? ''
+      if (metaTags === '') return html
+
+      return html.replace('</head>', `${metaTags}</head>`)
+    }
+  }
+}
