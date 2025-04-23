bb7e45efcbbc288f47621951238ffdc0b4cff283
Kaido Iwamoto <kaido@odiak.net>
2025-04-24 05:52:22 +0900

    Refactor CanvasBottomMenu

--- src/components/CanvasBottomMenu.tsx
+++ src/components/CanvasBottomMenu.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react'
-import styled from 'styled-components'
 import { withPrefix } from '../i18n/translate'
 import { BottomMenuState } from './Canvas'
 
@@ -43,31 +42,24 @@ export const CanvasBottomMenu: FC<Props> = ({ state, onCopy, onPaste, onDelete }
   }
 }
 
-const BottomMenu = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: 0;
-  padding: 0;
-  padding-bottom: 16px;
-  padding-bottom: calc(env(safe-area-inset-bottom, 0px) + 16px);
-  text-align: center;
-`
+const BottomMenu: FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 m-0 p-0 pb-[calc(env(safe-area-inset-bottom,0px)+16px)] text-center space-x-[5px]">
+      {children}
+    </div>
+  )
+}
 
-const BottomMenuItem = styled.button`
-  margin: 0;
-  padding: 5px;
-  display: inline-block;
-  background: #aaab;
-  border: 0;
-  border-radius: 2px;
-  color: inherit;
-  & + & {
-    margin-left: 5px;
-  }
-
-  @media (prefers-color-scheme: dark) {
-    background: #666b;
-  }
-`
+const BottomMenuItem: FC<{ onClick?: () => void; children: React.ReactNode }> = ({
+  onClick,
+  children
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className="p-[5px] inline-block bg-[#aaab] border-0 rounded-[2px] text-inherit dark:bg-[#666b]"
+    >
+      {children}
+    </button>
+  )
+}
